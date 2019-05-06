d2e22970fad699686cc441a077b95373811d6953
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2019-05-06 17:38:40 +0530

    Add files via upload

---
+++ esp/esp.ino
@@ -0,0 +1,48 @@
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+ 
+
+const uint8_t scl = 2; //D5
+const uint8_t sda = 0; //D6
+ 
+MPU6050 mpu;
+ 
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+ 
+void setup()
+{
+Wire.begin(sda, scl);
+Serial.begin(115200);
+ Serial.print("Initializing.");
+  for(int i=1;i<6;++i)
+  { 
+    mux(i);
+    Serial.print("..");
+    delay(10);
+    mpu.initialize();
+  }
+  Serial.println("Initialization Successful");
+
+}
+ 
+void loop()
+{
+
+ for(int i=1;i<9;++i)
+ { 
+  mux(i);
+  delay(1);
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  Serial.print(i);
+  Serial.print(" = ");
+  Serial.print(ay);
+  Serial.print(" || ");
+  if(i==8)
+{
+    Serial.println();
+    delay(10);
+}
+
+}}
---
+++ esp/setup.ino
@@ -0,0 +1,48 @@
+void mux(int i)
+{
+  
+  switch(i)
+  {
+    case 6:
+    digitalWrite(5,0);
+    digitalWrite(6,0);
+    digitalWrite(7,0);
+    break;
+    case 1:
+    digitalWrite(5,1);
+    digitalWrite(6,0);
+    digitalWrite(7,0);
+    break;
+    case 7:
+    digitalWrite(5,0);
+    digitalWrite(6,1);
+    digitalWrite(7,0);
+    break;
+    case 2:
+    digitalWrite(5,1);
+    digitalWrite(6,1);
+    digitalWrite(7,0);
+    break;
+    case 8:
+    digitalWrite(5,0);
+    digitalWrite(6,0);
+    digitalWrite(7,1);
+    break;
+    case 3:
+     digitalWrite(5,1);
+    digitalWrite(6,1);
+    digitalWrite(7,1);
+    break;
+    case 4:
+    digitalWrite(5,1);
+    digitalWrite(6,0);
+    digitalWrite(7,1);
+    break;
+    case 5:
+    digitalWrite(5,0);
+    digitalWrite(6,1);
+    digitalWrite(7,1);
+    break;
+  }
+  
+}
